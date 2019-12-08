7adcfb89c1be27de19ad1265b1c71849c2d4ad54
Beto <beto@minify.com.br>
2019-12-08 03:51:12 -0300

    Modify typography's theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,23 @@
 import Typography from 'typography';
 import doelgerTheme from 'typography-theme-doelger';
 
+doelgerTheme.overrideThemeStyles = () => {
+  return {
+    html: {
+      lineHeight: `1.53em`
+    },
+    p: {
+      marginBottom: `1.53rem`
+    }
+  };
+};
+
 const typography = new Typography(doelgerTheme);
+
+// Hot reload typography in development.
+if (process.env.NODE_ENV !== `production`) {
+  typography.injectStyles();
+}
 // Export helper functions
 export const { scale, rhythm, options } = typography;
 export default typography;
