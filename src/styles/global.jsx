f7108e87c539ff178f0227bd6cd855c575ab988c
Beto <beto@minify.com.br>
2019-12-08 03:51:27 -0300

    Remove fonts settings from global styles

--- src/styles/global.jsx
+++ src/styles/global.jsx
@@ -5,6 +5,7 @@ const GlobalStyles = createGlobalStyle`
    v2.0 | 20110126
    License: none (public domain)
   */
+
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
@@ -18,19 +19,13 @@ const GlobalStyles = createGlobalStyle`
   figure, figcaption, footer, header, hgroup,
   menu, nav, output, ruby, section, summary,
   time, mark, audio, video {
-    margin: 0;
     padding: 0;
     border: 0;
-    font-size: 100%;
-    vertical-align: baseline;
   }
   /* HTML5 display-role reset for older browsers */
   article, aside, details, figcaption, figure,
   footer, header, hgroup, menu, nav, section {
     display: block;
-  }
-  body {
-    line-height: 1;
   }
   ol, ul {
     list-style: none;
@@ -52,13 +47,12 @@ const GlobalStyles = createGlobalStyle`
   }
   body {
     background: #fff;
-    line-height: 1;
-    font-size: 100%;
   }
   img {
     display: block;
   	width: 100%;
-  	height: auto;
+    height: auto;
+    margin: 0;
   }
   /* body.dark {
     --borders: #38444d;
