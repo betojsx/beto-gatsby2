4d4b103ab162d403befb98ec61eacdc6caa43343
Beto <beto@minify.com.br>
2019-12-10 22:34:27 -0300

    Adiciona favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,18 +87,7 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: 'minimal-ui',
-        icons: [
-          {
-            src: '/logos/logo-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/logos/logo-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
+        icon: 'static/icons/favicon-32x32.png'
       }
     },
     'gatsby-plugin-offline',
